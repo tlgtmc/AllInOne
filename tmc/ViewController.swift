d128575d7fe511268dccbc509627b4fa73641810
Tolga Atmaca <tlg.tmc@gmail.com>
2020-03-10 21:48:48 +0300

    Initial Commit

---
+++ tmc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tmc
+//
+//  Created by Tolga Atmaca on 10.03.2020.
+//  Copyright © 2020 Tolga Atmaca. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
